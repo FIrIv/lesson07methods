53f62b88d829b4a8fa75a875d76679b2f71ce045
student <i.i.f@mail.ru>
2022-03-23 22:08:18 +0800

    1, 2, 3 задания

--- src/test/skypro/Main.java
+++ src/test/skypro/Main.java
@@ -6,23 +6,24 @@ public class Main {
     public static void main(String[] args) {
 	    // задание 1
         int year = 2020;
-        System.out.println(testYear(year));
+        System.out.println(year + " : " + testYear(year));
 
         // задание 2
         // 0 — iOS или 1 — Android
         byte clientOS = 0;
-        int clientDeviceYear = 2015;
-        System.out.println(testOS(clientOS, clientDeviceYear));
+        int clientDeviceYear = 2019;
+        System.out.println(clientDeviceYear + " : " + testOS(clientOS, clientDeviceYear));
 
         // задание 3
-
+        int distance = 60;
+        System.out.println(distance + " км : " + calculateDeliveryDays (distance));
     }
 
     public static String testYear(int year) {
         if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)){
-            return(year + " - не високосный год. ");
+            return("Не високосный год. ");
         } else {
-            return(year + " - високосный год. ");
+            return("Високосный год. ");
         }
     }
 
@@ -43,5 +44,14 @@ public class Main {
         } else {
             return("Ошибка");
         }
+    }
+
+    public static String calculateDeliveryDays (int distance) {
+        // <=20км - 1 день, <=60км - 2 дня, <=100км - 3 дня и т.д.
+        int days = 1;
+        if (distance > 20 && distance <=60) days = 2;
+        else if (distance > 60 && distance <=100) days = 3;
+        else if (distance > 100) return ("Доставка более 100км не производится. ");
+        return "Доставка " + days + "дн.";
     }
 }
